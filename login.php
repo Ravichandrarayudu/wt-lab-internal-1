e066e96f06419434eec46537bb59154e9a4c6e01
Ravichandrarayudu <37443137+Ravichandrarayudu@users.noreply.github.com>
2018-03-16 17:28:52 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,28 @@
+
+<html>
+<head>
+	<title>login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h2>Login</h2>
+	</div>
+	<form method="post" action="">
+			<label>Username</label>
+			<input type="text" name="username" placeholder="username">
+			<br></br>
+			<label>Password</label>
+			<br></br>
+			<input type="password" name="password1" placeholder="password">
+			<br></br>
+			<button type="submit" name="Login" class="btn">Login</button>
+		<p>
+			Not yet registered? <a href="register.php">Sign up</a>
+		</p>
+		
+	</form>
+	
+	</body>
+</html>
+
---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type=text/css href="style.css"/>
+</head>
+<body>
+
+<div class="container">
+    <h1>Sign Up</h1>
+    <p>fill the form to create an account.</p>
+    <hr>
+    <label for="Username"><b>username</b></label>
+    <input type="text" placeholder="Enter Username" name="username">
+    <br></br>
+    <label for="Email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email">
+    <br></br>
+    <label for="Password"><b>password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw">
+    <br></br>
+    <label for="Repeat Password"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="psw-repeat">
+    <br></br>
+     <label for="Mobile number"><b>Mobile number</b></label>
+    <input type="text" placeholder="Enter Mobile number" name="mobile number">
+    <br></br>
+      <button type="submit" class="signup">Sign Up</button>
+  </div>
+</body>
+</html>
+
